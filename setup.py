774e7d62de2c6d04d302e9470a4299c329642e92
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2016-11-05 16:54:00 -0400

    Add python setup config

---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) Gonzalo Peña-Castellanos (@goanpeca)
+#
+# Licensed under the terms of the MIT License
+# (See LICENSE.txt for details)
+# -----------------------------------------------------------------------------
+"""Appveyor Python Client."""
+
+# Standard library imports
+import ast
+import os
+
+# Third party imports
+from setuptools import find_packages, setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version(module='appveyor_client'):
+    """Get version."""
+    with open(os.path.join(HERE, module, '__init__.py'), 'r') as f:
+        data = f.read()
+    lines = data.split('\n')
+    for line in lines:
+        if line.startswith('VERSION_INFO'):
+            version_tuple = ast.literal_eval(line.split('=')[-1].strip())
+            version = '.'.join(map(str, version_tuple))
+            break
+    return version
+
+
+def get_description():
+    """Get long description."""
+    with open(os.path.join(HERE, 'README.rst'), 'r') as f:
+        data = f.read()
+    return data
+
+
+setup(
+    name='appveyor-client',
+    version=get_version(),
+    keywords=["appveyor client continuous integration api rest ci"],
+    url='https://github.com/goanpeca/appveyor-client',
+    license='MIT',
+    author='Gonzalo Peña-Castellanos',
+    author_email='goanpeca@gmail.com',
+    maintainer='Gonzalo Peña-Castellanos',
+    maintainer_email='goanpeca@gmail.com',
+    description='Appveyor Python Client',
+    long_description=get_description(),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ])
